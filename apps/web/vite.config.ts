eff95b0e8c988b1d9d77262f872699c8acabb317
Daz-za <36137203+Daz-za@users.noreply.github.com>
2026-04-09 15:11:12 +0200

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,10 @@
-{
-  "extends": "../../tsconfig.base.json",
-  "compilerOptions": {
-    "jsx": "react-jsx",
-    "types": ["vite/client"]
-  },
-  "include": ["src"]
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000
+  }
+});
